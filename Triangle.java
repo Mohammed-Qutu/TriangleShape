7cf306a9eb9a7312b060b7bab40954326c39ca6f
Mohammed-Qutu <q2mohammed@gmail.com>
2024-08-21 14:23:57 -0500

    Create Triangle.java
    Added Triangle.java file

---
+++ Triangle.java
@@ -0,0 +1,15 @@
+/* This program will crate a Triangle shape
+Written by Mohammed Qutu 1/18/2024
+21.0.2; Java HotSpot(TM) 64-Bit Server VM 21.0.2+13-LTS-58
+*/
+public class Triangle {
+
+    public static void main(String[] args) {
+        System.out.println("           *");
+        System.out.println("          ***");
+        System.out.println("         *****");
+        System.out.println("        *******");
+        System.out.println("       *********");
+        System.out.println("      ***********");
+    }
+}
